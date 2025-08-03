134a03128f40b6245f59c606856bf853bc2766e8
yepes0 <71985093+yepes0@users.noreply.github.com>
2025-08-03 04:10:04 +0000

    got it working again

--- dice.rb
+++ dice.rb
@@ -9,54 +9,38 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "
-  <h1>Dice Roll</h1>
-  <ul>
-    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-  </ul>
-  "
+  erb(:elephant)
 end
 
-get("/zebra") do
-  "We must add a route for each path we want to support"
-end
-
-get("/giraffe") do
-  "Hopefully this shows up without having to restart the server 🤞🏾"
-end
 
 get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
-  
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-  
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  erb(:two_six)
 end
+
 
 get("/dice/2/10") do
   first_die = rand(1..10)
   second_die = rand(1..10)
   sum = first_die + second_die
   
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
   
-  "<h1>2d10</h1>
-   <p>#{outcome}</p>"
+      erb(:two_ten)
+
 end
 
 get("/dice/1/20") do
   die = rand(1..20)
   
-  outcome = "You rolled a #{die}."
+  @outcome = "You rolled a #{die}."
   
-  "<h1>1d20</h1>
-   <p>#{outcome}</p>"
+    erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -66,8 +50,7 @@ get("/dice/5/4") do
   fourth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die
   
-  outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
   
-  "<h1>5d4</h1>
-   <p>#{outcome}</p>"
+    erb(:five_four)
 end
