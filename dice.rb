e430965e29cb3da399900e06728d3e4df9ccc610
yepes0 <71985093+yepes0@users.noreply.github.com>
2025-08-03 02:09:46 +0000

    listo papi

--- dice.rb
+++ dice.rb
@@ -1,10 +1,16 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+  
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
   "
   <h1>Dice Roll</h1>
-
   <ul>
     <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
     <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
@@ -14,9 +20,12 @@ get("/") do
   "
 end
 
-
 get("/zebra") do
-  "Wuddup Zebra!"
+  "We must add a route for each path we want to support"
+end
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
 end
 
 get("/dice/2/6") do
@@ -34,20 +43,31 @@ get("/dice/2/10") do
   first_die = rand(1..10)
   second_die = rand(1..10)
   sum = first_die + second_die
+  
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
-
-  "<h1> 2d10</h1>
-  <p>#{outcome}</p>"
+get("/dice/1/20") do
+  die = rand(1..20)
+  
+  outcome = "You rolled a #{die}."
+  
+  "<h1>1d20</h1>
+   <p>#{outcome}</p>"
 end
 
 get("/dice/5/4") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
-
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
-
-  "<h1> 2d10</h1>
-  <p>#{outcome}</p>"
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die
+  
+  outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+  
+  "<h1>5d4</h1>
+   <p>#{outcome}</p>"
 end
