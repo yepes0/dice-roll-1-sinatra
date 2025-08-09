603bb782843bacdde47ce90c16dedc71350dedd2
yepes0 <71985093+yepes0@users.noreply.github.com>
2025-08-09 23:58:48 +0000

    matching but before making css updates

--- dice.rb
+++ dice.rb
@@ -21,39 +21,40 @@ get("/dice/2/6") do
     @rolls.push(dice)
   end
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-
   erb(:two_six)
 end
 
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
-  
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-  
-      erb(:two_ten)
+  @rolls = []
+
+  2.times do
+    dice = rand(1..10)
+    @rolls.push(dice)
+  end
+
+  erb(:two_ten)
 
 end
 
 get("/dice/1/20") do
-  @die = rand(1..20)
-  
-  @outcome = "You rolled a #{@die}."
+  @rolls = []
+
+  1.times do
+    die = rand(1..20)
+    @rolls.push(die)
+  end
   
     erb(:one_twenty)
 end
 
 get("/dice/5/4") do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die
-  
-  @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+  @rolls = []
+
+  5.times do
+  dice = rand(1..4)
+  @rolls.push(dice)
+  end
   
     erb(:five_four)
 end
